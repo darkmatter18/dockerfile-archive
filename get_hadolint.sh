46bedf44a85ef1d040f3a5b9599af133ff1ec812
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2022-10-19 20:41:34 +0000

    added hadolint config to run on all the repository

---
+++ get_hadolint.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64
+mv hadolint-Linux-x86_64 hadolint
+chmod +x hadolint
---
+++ run_linter.sh
@@ -0,0 +1,1 @@
+find . -name Dockerfile |xargs ./hadolint
